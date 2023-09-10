4ab2b53ad7b6aa1c9f25dd8545fe97a2d4004cf9
Corey Psoinos <coreypsoinos@gmail.com>
2023-09-10 11:20:30 -0400

    use unlinkSync instead of fs/promises

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import 'dotenv/config'
 import { captureImage } from './captureImage'
 import { putObject } from './r2Client'
-import { unlink } from 'fs/promises'
+import { unlinkSync } from 'fs'
 
 const main = async () => {
   const { filename, location, result } = await captureImage()
 
   const uploadResult = await putObject(`images/${filename}`, result, 'image/jpeg')
   console.log(uploadResult)
-  await unlink(location)
+  unlinkSync(location)
 }
 
 main()
