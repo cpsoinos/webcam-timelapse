f1e9b05298cef75528e63532970ac4b825549d8c
Corey Psoinos <coreypsoinos@gmail.com>
2023-09-10 11:14:20 -0400

    configure tsup to output cjs with all deps bundled

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig((options) => ({
+  entry: ['./src/main.ts'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  format: ['cjs'],
+  noExternal: ['@anthonylzq/node-webcam', '@aws-sdk/client-s3'],
+  minify: !options.watch
+}))
